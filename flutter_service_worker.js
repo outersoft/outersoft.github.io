5e7b6d8e2347b20976d0eb2f120f66100e948856
danger <degjeg@163.com>
2021-07-08 23:23:33 +0800

    render canvaskit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "9811edcf9b093e66f5caed606bd31c05",
-"index.html": "0cd5e8e4e16453a3075bdbcff3b5c5f8",
-"/": "0cd5e8e4e16453a3075bdbcff3b5c5f8",
-"main.dart.js": "4bdfd02636dbb3896ad35d397d7af180",
+"index.html": "056b856cd463eb525b126e32902d938f",
+"/": "056b856cd463eb525b126e32902d938f",
+"main.dart.js": "6ca3fa82b84c9370d14959247367b0f1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
